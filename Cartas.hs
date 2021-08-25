2da00cf4899227657c712a0ba0a696ac6ec2e416
icaro-chagas <icaro.chagas.a@gmail.com>
2021-08-25 09:16:26 -0300

    Versão inicial

---
+++ Cartas.hs
@@ -0,0 +1,85 @@
+module Cartas ( Carta(..), baralho) where  
+
+import System.Random
+import Data.Array.IO
+import Control.Monad
+
+{-data Carta = Zero {cor :: String} | Um {cor :: String} | Dois {cor :: String} | Tres {cor :: String} | Quatro {cor :: String}
+           | Cinco {cor :: String} | Seis {cor :: String} | Sete {cor :: String} | Oito {cor :: String} | Nove {cor :: String}  
+           | Mais2 {cor :: String} | Inverter {cor :: String} | Pular {cor :: String} | Coringa | CoringaMais4 deriving (Eq)-}
+           
+data Carta = Carta {tipo :: String, cor :: String} deriving (Eq)
+
+cartasZero :: [Carta]
+cartasZero = [Carta "Zero" "Azul", Carta "Zero" "Verde", Carta "Zero" "Amarela", Carta "Zero" "Vermelha"]
+
+cartasUm :: [Carta]
+cartasUm = [Carta "Um" "Azul", Carta "Um" "Azul", Carta "Um" "Verde", Carta "Um" "Verde", 
+            Carta "Um" "Amarela", Carta "Um" "Amarela", Carta "Um" "Vermelha", Carta "Um" "Vermelha"]
+
+cartasDois :: [Carta]
+cartasDois = [Carta "Dois" "Azul", Carta "Dois" "Azul", Carta "Dois" "Verde", Carta "Dois" "Verde", 
+              Carta "Dois" "Amarela", Carta "Dois" "Amarela", Carta "Dois" "Vermelha", Carta "Dois" "Vermelha"]
+
+cartasTres :: [Carta]
+cartasTres = [Carta "Tres" "Azul", Carta "Tres" "Azul", Carta "Tres" "Verde", Carta "Tres" "Verde", 
+              Carta "Tres" "Amarela", Carta "Tres" "Amarela", Carta "Tres" "Vermelha", Carta "Tres" "Vermelha"]
+
+cartasQuatro :: [Carta]
+cartasQuatro = [Carta "Quatro" "Azul", Carta "Quatro" "Azul", Carta "Quatro" "Verde", Carta "Quatro" "Verde", 
+                Carta "Quatro" "Amarela", Carta "Quatro" "Amarela", Carta "Quatro" "Vermelha", Carta "Quatro" "Vermelha"]
+
+cartasCinco :: [Carta]
+cartasCinco = [Carta "Cinco" "Azul", Carta "Cinco" "Azul", Carta "Cinco" "Verde", Carta "Cinco" "Verde",  
+               Carta "Cinco" "Amarela", Carta "Cinco" "Amarela", Carta "Cinco" "Vermelha", Carta "Cinco" "Vermelha"]
+
+cartasSeis :: [Carta]
+cartasSeis = [Carta "Seis" "Azul", Carta "Seis" "Azul", Carta "Seis" "Verde", Carta "Seis" "Verde", 
+              Carta "Seis" "Amarela", Carta "Seis" "Amarela", Carta "Seis" "Vermelha", Carta "Seis" "Vermelha"]
+
+cartasSete :: [Carta]
+cartasSete = [Carta "Sete" "Azul", Carta "Sete" "Azul", Carta "Sete" "Verde", Carta "Sete" "Verde", 
+              Carta "Sete" "Amarela", Carta "Sete" "Amarela", Carta "Sete" "Vermelha", Carta "Sete" "Vermelha"]
+
+cartasOito :: [Carta]
+cartasOito = [Carta "Oito" "Azul", Carta "Oito" "Azul", Carta "Oito" "Verde", Carta "Oito" "Verde", 
+              Carta "Oito" "Amarela", Carta "Oito" "Amarela", Carta "Oito" "Vermelha", Carta "Oito" "Vermelha"]
+
+cartasNove :: [Carta]
+cartasNove = [Carta "Nove" "Azul", Carta "Nove" "Azul", Carta "Nove" "Verde", Carta "Nove" "Verde", 
+              Carta "Nove" "Amarela", Carta "Nove" "Amarela", Carta "Nove" "Vermelha", Carta "Nove" "Vermelha"]
+                       
+cartasMais2 :: [Carta]
+cartasMais2 = [Carta "Mais2" "Azul", Carta "Mais2" "Azul", Carta "Mais2" "Verde", Carta "Mais2" "Verde", 
+               Carta "Mais2" "Amarela", Carta "Mais2" "Amarela", Carta "Mais2" "Vermelha", Carta "Mais2" "Vermelha"]
+
+cartasInverter :: [Carta]
+cartasInverter = [Carta "Inverter" "Azul", Carta "Inverter" "Azul", Carta "Inverter" "Verde", Carta "Inverter" "Verde", 
+                  Carta "Inverter" "Amarela", Carta "Inverter" "Amarela", Carta "Inverter" "Vermelha", Carta "Inverter" "Vermelha"]
+
+cartasPular :: [Carta]
+cartasPular = [Carta "Pular" "Azul", Carta "Pular" "Azul", Carta "Pular" "Verde", Carta "Pular" "Verde", 
+               Carta "Pular" "Amarela", Carta "Pular" "Amarela", Carta "Pular" "Vermelha", Carta "Pular" "Vermelha"]
+
+cartasCoringa :: [Carta]
+cartasCoringa = [Carta "Coringa" "Preta", Carta "Coringa" "Preta", Carta "Coringa" "Preta", Carta "Coringa" "Preta"]
+
+cartasCoringaMais4 :: [Carta]
+cartasCoringaMais4 = [Carta "CoringaMais4" "Preta", Carta "CoringaMais4" "Preta", Carta "CoringaMais4" "Preta", Carta "CoringaMais4" "Preta"]
+
+
+baralho :: [Carta]
+baralho = cartasZero ++ cartasUm ++ cartasDois ++ cartasTres ++ cartasQuatro ++ cartasCinco ++ 
+          cartasSeis ++ cartasSete ++ cartasOito ++ cartasNove ++ cartasInverter ++ cartasPular ++
+          cartasCoringa ++ cartasCoringaMais4
+          
+{-getBaralho :: [Carta] -> IO [Carta]
+getBaralho b = shuffle b-}
+          
+instance Show Carta where  
+    show Carta {tipo = a, cor = b} = a ++ " - " ++ b
+    
+
+
+    
+
---
+++ Teste2.hs
@@ -0,0 +1,207 @@
+import Cartas
+import System.Random
+import Data.Array.IO
+import Control.Monad
+import Data.List
+import qualified System.Process as SP
+
+main = do
+    
+    shuffledDeck <- shuffle baralho
+    
+    {-let jogador1 = criaJogador 0 shuffledDeck
+    
+    let shuffledDeck1 = atualizaBaralho True 4 shuffledDeck
+    
+    let shuffledDeck = shuffledDeck1
+    
+    let jogador2 = criaJogador 0 shuffledDeck
+    
+    let shuffledDeck1 = atualizaBaralho True 4 shuffledDeck
+    
+    let shuffledDeck = shuffledDeck1-}
+    
+    SP.system "clear"
+    putStr "Jogador 1: "
+    print jogador1
+    putStr "Jogador 2: "
+    print jogador2
+    print mesa 
+    
+    atualizaJogo mesa jogador1 jogador2 shuffledDeck  
+       
+
+atualizaJogo mesaInit jogador1 jogador2 shuffledDeck = do
+     
+    if length jogador1 == 0 || length jogador2 == 0   
+        then do putStr "Jogador 1: "
+                print jogador1
+                putStr "Jogador 2: "
+                print jogador2 
+        else do jogaJogador1 mesaInit jogador1 jogador2 shuffledDeck                            
+
+jogaJogador1 mesaInit jogador1 jogador2 shuffledDeck = do
+    -- Tamanho da lista do jogador 1 antes de tentar jogar uma carta
+    let lenJ11 = length jogador1
+    
+    line <- getLine
+    SP.system "clear"
+    let mesa2 = atualizaMesa mesaInit jogador1 (length jogador2)
+    
+    putStr "Jogador 1: "
+    print jogador1
+    print mesa2
+    
+    let auxJogador1 = jogaCarta mesa2 jogador1 (length jogador2)
+    let jogador1 = auxJogador1
+    
+    -- Tamanho da lista do jogador 1 depois de tentar jogar uma carta
+    let lenJ12 = length jogador1
+    
+    if (tipo mesa2 == "Pular")
+        then do jogaJogador1 mesa2 jogador1 jogador2 shuffledDeck
+    
+    else if (lenJ11 == lenJ12)
+        then do incementaMaoJogador1 mesa2 jogador1 jogador2 shuffledDeck
+        
+        else do jogaJogador2 mesa2 jogador1 jogador2 shuffledDeck
+    
+    {-if (tipo mesa2 == "pular")
+        then do jogaJogador1 mesa2 jogador1 jogador2 shuffledDeck
+      
+        else do return()-}
+    
+incementaMaoJogador1 mesaInit jogador1 jogador2 shuffledDeck = do
+    
+    line <- getLine
+    SP.system "clear"
+    let auxJogador1 = incrementaMao True jogador1 shuffledDeck
+    let jogador1 = auxJogador1
+    
+    let auxShuffledDeck = atualizaBaralho True 1 shuffledDeck
+    let shuffledDeck = auxShuffledDeck
+    
+    let mesa2 = atualizaMesa mesaInit jogador1 (length jogador2)
+    
+    putStr "Jogador 1 (Atualizado): "
+    print jogador1
+    print mesa2
+    
+    let auxJogador1 = jogaCarta mesa2 jogador1 (length jogador2)
+    let jogador1 = auxJogador1
+    
+    if ((mesaInit /= mesa2) && (tipo mesa2 == "Pular"))
+        then do jogaJogador1 mesa2 jogador1 jogador2 shuffledDeck
+      
+        else do jogaJogador2 mesa2 jogador1 jogador2 shuffledDeck
+
+jogaJogador2 mesaInit jogador1 jogador2 shuffledDeck = do
+
+    -- Tamanho da lista do jogador 2 antes de tentar jogar uma carta
+    let lenJ21 = length jogador2
+    
+    line <- getLine
+    SP.system "clear"
+    let mesa2 = atualizaMesa mesaInit jogador2 (length jogador1)
+
+    putStr "Jogador 2: "
+    print jogador2
+    print mesa2
+
+    let auxJogador2 = jogaCarta mesa2 jogador2 (length jogador1)
+    let jogador2 = auxJogador2
+    
+    -- Tamanho da lista do jogador 2 depois de tentar jogar uma carta
+    let lenJ22 = length jogador2
+    
+    if ((mesaInit /= mesa2) && (tipo mesa2 == "Pular"))
+        then do jogaJogador2 mesa2 jogador1 jogador2 shuffledDeck
+    
+    else if (lenJ21 == lenJ22)
+        then do incementaMaoJogador2 mesa2 jogador1 jogador2 shuffledDeck
+        
+        else do atualizaJogo mesa2 jogador1 jogador2 shuffledDeck
+        
+    {-if (tipo mesa2 == "Pular")
+        then do jogaJogador2 mesa2 jogador1 jogador2 shuffledDeck
+      
+        else do return()-}
+    
+incementaMaoJogador2 mesaInit jogador1 jogador2 shuffledDeck = do
+    
+    line <- getLine
+    SP.system "clear"
+    let auxJogador2 = incrementaMao True jogador2 shuffledDeck
+    let jogador2 = auxJogador2
+    
+    let auxShuffledDeck = atualizaBaralho True 1 shuffledDeck
+    let shuffledDeck = auxShuffledDeck
+    
+    let mesa2 = atualizaMesa mesaInit jogador2 (length jogador1)
+    
+    putStr "Jogador 2 (Atualizado): "
+    print jogador2
+    print mesa2
+    
+    let auxJogador2 = jogaCarta mesa2 jogador2 (length jogador1)
+    let jogador2 = auxJogador2
+    
+    if ((mesaInit /= mesa2) && (tipo mesa2 == "Pular"))
+        then do jogaJogador2 mesa2 jogador1 jogador2 shuffledDeck
+      
+        else do atualizaJogo mesa2 jogador1 jogador2 shuffledDeck
+
+
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+  where
+    n = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs =  newListArray (1,n) xs
+
+jogaCarta :: Carta -> [Carta] -> Int -> [Carta]
+jogaCarta _ (x:xs) 0 = (x:xs)
+jogaCarta c (x:xs) n = delete c (x:xs) 
+    
+criaJogador :: Int -> [Carta] -> [Carta]
+criaJogador 4 _ = []
+criaJogador a (x:xs) = x: criaJogador (a+1) xs
+
+jogador1 :: [Carta]
+jogador1 = [Carta "Um" "Amarela",Carta "Tres" "Amarela", Carta "Dois" "Vermelha", Carta "Dois" "Verde"]
+
+jogador2 :: [Carta]
+jogador2 = [Carta "Pular" "Vermelha", Carta "Tres" "Verde", Carta "Pular" "Verde", Carta "Dois" "Verde"]
+
+
+mesa :: Carta 
+mesa = Carta "Um" "Azul"
+
+atualizaMesa :: Carta -> [Carta] -> Int -> Carta
+atualizaMesa (Carta t c) _ 0 = Carta "" "" 
+atualizaMesa (Carta t c) [] a = (Carta t c)
+atualizaMesa (Carta t c) (head:tail) a
+        | (t == tipo head) || (c == cor head) = head
+        | otherwise =  atualizaMesa (Carta t c) tail a
+
+atualizaBaralho :: Bool -> Int -> [Carta] -> [Carta]
+atualizaBaralho True n (x:xs) = drop n (x:xs)
+atualizaBaralho False _ (x:xs) = (x:xs)
+
+incrementaMao :: Bool -> [Carta] -> [Carta] -> [Carta]
+incrementaMao True (x:xs) (y:ys) = y:(x:xs)
+incrementaMao False (x:xs) _ = (x:xs) 
+
+
+
+
+
+
+ 
